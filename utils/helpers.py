88ec500a024e7bfdff79f9e65cd7427e66c74ab6
Prashobh Paul <prashobhpaulnambadan@gmail.com>
2025-05-25 03:46:11 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,3 +7,11 @@ def load_user_data():
     users = {row[0]: {"interests": row[1].split(","), "role": row[2], "calendar": {}, "registered_sessions": []} for row in c.fetchall()}
     conn.close()
     return users
+    
+def load_faqs():
+    return {
+        "when is the event": "The event is on 25-27 May 2025.",
+        "where is the event": "The event is happening at International Convention Center.",
+        "what are the guidelines": "Please carry your badge and follow COVID protocols.",
+        "who are the speakers": "We have 20+ speakers from various industries including Dr. A and Dr. B."
+    }
