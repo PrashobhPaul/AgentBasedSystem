270499ae1054447553afd479c5489278bb78f3dc
Prashobh Paul <prashobhpaulnambadan@gmail.com>
2025-05-25 03:07:00 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,14 @@
-db data
+import sqlite3
+
+def get_connection():
+    return sqlite3.connect("event.db")
+
+def query_db(query, args=(), one=False):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(query, args)
+    rv = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return (rv[0] if rv else None) if one else rv
+
